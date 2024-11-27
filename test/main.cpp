886a5b7e006cd80f78ce28ada3534d694518d4f9
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-11-27 23:59:27 +0800

    Add CMakeLists

---
+++ test/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <memory>
+#include <array>
+#include <cstdio>
+
+int main() {
+    std::array<char, 128> buffer;
+    std::string result;
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("sudo bash test-root.sh", "r"), pclose);
+    if (!pipe) {
+        std::cerr << "Failed to run script." << std::endl;
+        return 1;
+    }
+
+    int status = 0;
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+
+    status = pclose(pipe.get());
+
+    if (status != 0) {
+        std::cerr << "Script failed with exit status: " << status << std::endl;
+        return 1;
+    }
+
+    std::cout << result;
+    return 0;
+}
