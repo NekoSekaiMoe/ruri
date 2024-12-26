ac5c276be57448dcf9bba55257cc5b13e0175bbb
Ayayoku Togetsu <130299352+dpkg123@users.noreply.github.com>
2024-12-26 20:44:50 +0800

    更新 arm64.sh

--- arm64.sh
+++ arm64.sh
@@ -32,7 +32,7 @@ sudo cat | sudo tee -a aarch64/build.sh <<EOF
 #!/bin/bash
 set -x
 apt update
-apt install dpkg-dev git upx strip -y
+apt install dpkg-dev git upx binutils -y
 git clone https://github.com/dpkg123/ruri --depth=1
 cd ruri
 apt build-dep . -y
