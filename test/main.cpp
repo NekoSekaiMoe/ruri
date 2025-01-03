e4f48f784f594a8f8c55255580848d2ad52dea7b
dpkg123 <test1@qw5d.onmicrosoft.com>
2025-01-03 11:50:30 +0800

    Fix Makefile.test

--- test/main.cpp
+++ test/main.cpp
@@ -181,8 +181,6 @@ int main() {
     register_signal();
     std::array<char, 128> buffer;
     std::string result;
-    int* ptr = nullptr;  // 空指针
-    *ptr = 42;  
     execute_script("./test-root.sh");
     return 0;
 }
