fcd75df52104488e7fe65de883ccd945f013e64b
dpkg123 <test1@qw5d.onmicrosoft.com>
2025-04-03 00:01:58 +0800

    Update config.cache

--- test/log.sh
+++ test/log.sh
@@ -2,4 +2,5 @@
 PS4='+ [DEBUG] ${BASH_SOURCE}:${LINENO}: ' && export PS4
 set -e
 
-strace -f -e trace=write bash -x test-root.sh | tee -a log.txt
+#strace -f -e trace=write \
+bash -x test-root.sh | tee -a log.txt
